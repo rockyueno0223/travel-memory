f9f28941f91db70325812b5f9d0f42de222a5f5d
Hiroki Ueno <rockyueno@gmail.com>
2024-07-29 01:00:19 -0700

    updated worldMap style

--- components/WorldMap.tsx
+++ components/WorldMap.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import { ComposableMap, Geographies, Geography, GeographyProps, GeographyPaths } from 'react-simple-maps';
+import { ComposableMap, Geographies, Geography, GeographyProps, GeographyPaths, Sphere, Graticule } from 'react-simple-maps';
 
 const geoUrl = "https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json";
 
@@ -10,11 +10,16 @@ const countriesInDatabase = ['United States', 'Canada', 'Mexico', 'France', 'Ger
 
 const WorldMap: React.FC = () => {
   return (
-    <ComposableMap>
+    <ComposableMap projectionConfig={{
+      rotate: [-10, 0, 0],
+      scale: 147
+    }}>
+      <Sphere stroke="#E4E5E6" strokeWidth={0.5} />
+      <Graticule stroke="#E4E5E6" strokeWidth={0.5} />
       <Geographies geography={geoUrl}>
         {({ geographies }: GeographyPaths) =>
           geographies.map((geo: any) => {
-            const countryName = geo.properties.NAME;
+            const countryName = geo.properties.name;
             const isInDatabase = countriesInDatabase.includes(countryName);
 
             return (
@@ -23,10 +28,12 @@ const WorldMap: React.FC = () => {
                 geography={geo}
                 //onClick={() => handleCountryClick(geo)}
                 style={{
-                  default: { fill: isInDatabase ? '#00F' : '#D6D6DA' }, // Blue for countries in the database, light grey for others
-                  hover: { fill: '#F53' },
-                  pressed: { fill: '#E42' },
+                  default: { fill: isInDatabase ? '#1FA2D5' : '#E5F4F7'}, // Blue for countries in the database, light grey for others
+                  hover: { fill: '#095A8C' },
+                  pressed: { fill: '#095A8C' },
                 }}
+                stroke='#fff'
+                strokeWidth={0.5}
               />
             );
           })
