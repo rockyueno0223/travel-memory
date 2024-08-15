64d3a753910bd96dbbb705e2f472989f8a5de482
Hiroki Ueno <rockyueno@gmail.com>
2024-08-14 22:17:46 -0700

    updated main visual and added images

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,40 @@
-import DeployButton from "../components/DeployButton";
-import AuthButton from "../components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
-import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
-import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
+import Link from "next/link";
 import Header from "@/components/Header";
 
 export default async function Index() {
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient();
-      return true;
-    } catch (e) {
-      return false;
-    }
-  };
-
-  const isSupabaseConnected = canInitSupabaseClient();
-
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          <DeployButton />
-          {isSupabaseConnected && <AuthButton />}
+    <div className="flex-1 w-full flex flex-col items-center">
+      <div className="w-full relative">
+        <picture>
+          <source
+            srcSet="/images/desktop/main-visual1-pc.jpg"
+            media="(min-width: 1024px)"
+          />
+          <source
+            srcSet="/images/mobile/main-visual1-sp.jpg"
+            media="(max-width: 1023px)"
+          />
+          <img
+            src="/images/mobile/main-visual1-sp.jpg"
+            alt="Temples"
+            className="w-full h-auto"
+          />
+        </picture>
+        <div className="absolute top-[36%] w-full flex justify-center">
+          <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold text-center">
+            Travel Memory
+          </h1>
         </div>
-      </nav>
-
-      <div className="flex-1 flex flex-col gap-20 max-w-4xl px-3">
-        <Header />
-        <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-        </main>
-      </div>
-
-      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-        <p>
-          Powered by{" "}
-          <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-            target="_blank"
-            className="font-bold hover:underline"
-            rel="noreferrer"
+        <div className="absolute top-[50%] w-full flex justify-center">
+          <Link
+            href="/login"
+            className="py-1 md:py-2 px-4 sm:px-5 md:px-6 text-base sm:text-lg md:text-xl border border-black rounded bg-transparent"
           >
-            Supabase
-          </a>
-        </p>
-      </footer>
+            Start Now
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
