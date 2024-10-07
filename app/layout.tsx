5a0c0449efa72a0cc20d9904f6b7ea4a8c18ee2a
Hiroki Ueno <rockyueno@gmail.com>
2024-10-06 21:24:02 -0700

    SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Travel Memory",
+  description: "Save your beautiful travels memories. Create account and save your photos of your travels.",
 };
 
 export default function RootLayout({
