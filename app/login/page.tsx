680cb3456b00db15a86085b406bdb8b4e27eaea4
Hiroki Ueno <rockyueno@gmail.com>
2024-10-06 21:46:14 -0700

    updated login style

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -55,22 +55,8 @@ export default function Login({
     <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
       <Link
         href="/"
-        className="absolute left-8 lg:left-16 top-24 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
+        className="absolute left-8 lg:left-16 top-24 py-2 px-6 rounded-md no-underline text-foreground border border-neutral-500 hover:bg-[#f0f0f0] flex items-center group text-sm"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
-        >
-          <polyline points="15 18 9 12 15 6" />
-        </svg>{" "}
         Back
       </Link>
 
@@ -96,14 +82,14 @@ export default function Login({
         />
         <SubmitButton
           formAction={signIn}
-          className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2"
+          className="border border-neutral-500 hover:bg-[#f0f0f0] rounded-md px-4 py-2 text-foreground mb-2"
           pendingText="Signing In..."
         >
           Sign In
         </SubmitButton>
         <SubmitButton
           formAction={signUp}
-          className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
+          className="border border-neutral-500 bg-slate-500 text-white hover:bg-slate-400 rounded-md px-4 py-2 text-foreground mb-2"
           pendingText="Signing Up..."
         >
           Sign Up
