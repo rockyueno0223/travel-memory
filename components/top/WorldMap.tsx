ac8c2d35f327deab775ceef81d222f47453809e6
Hiroki Ueno <rockyueno@gmail.com>
2024-10-06 17:35:55 -0700

    animation in world map

--- components/top/WorldMap.tsx
+++ components/top/WorldMap.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import { ComposableMap, Geographies, Geography, Sphere, Graticule } from 'react-simple-maps';
 import { UnCodesInDatabase } from '@/app/hooks/types';
+import { motion } from "framer-motion"
 
 const geoUrl = "https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json";
 
@@ -25,43 +26,50 @@ const WorldMap: React.FC<WorldMapProps> = ({ unCodesInDatabase, setTooltipConten
   }
 
   return (
-    <ComposableMap projectionConfig={{
-      rotate: [-10, 0, 0],
-      scale: 147
-    }}>
-      <Sphere id="rsm-sphere" fill="transparent" stroke="#E4E5E6" strokeWidth={0.5} />
-      <Graticule stroke="#E4E5E6" strokeWidth={0.5} />
-      <Geographies geography={geoUrl}>
-        {({ geographies }) =>
-          geographies.map((geo: any) => {
-            const isInDatabase = unCodesInDatabase.some(unCodeInDatabase => unCodeInDatabase.country_un_code === geo.id);
+    <motion.div
+      className='w-full'
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 2.0, delay: 0.5 }}
+    >
+      <ComposableMap projectionConfig={{
+        rotate: [-10, 0, 0],
+        scale: 147
+      }}>
+        <Sphere id="rsm-sphere" fill="transparent" stroke="#E4E5E6" strokeWidth={0.5} />
+        <Graticule stroke="#E4E5E6" strokeWidth={0.5} />
+        <Geographies geography={geoUrl}>
+          {({ geographies }) =>
+            geographies.map((geo: any) => {
+              const isInDatabase = unCodesInDatabase.some(unCodeInDatabase => unCodeInDatabase.country_un_code === geo.id);
 
-            return (
-              <Geography
-                key={geo.rsmKey}
-                geography={geo}
-                onMouseEnter={() => {
-                  setTooltipContent(`${geo.properties.name}`);
-                }}
-                onMouseLeave={() => {
-                  setTooltipContent("");
-                }}
-                onClick={() => handleMapClick(geo.id, isInDatabase)}
-                style={{
-                  default: { fill: isInDatabase ? '#1FA2D5' : '#E5F4F7'},
-                  hover: { fill: '#095A8C' },
-                  pressed: { fill: '#095A8C' },
-                }}
-                stroke='#fff'
-                strokeWidth={0.5}
-                data-tooltip-id="world-map-tooltip"
-                data-tooltip-content={geo.properties.name}
-              />
-            );
-          })
-        }
-      </Geographies>
-    </ComposableMap>
+              return (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  onMouseEnter={() => {
+                    setTooltipContent(`${geo.properties.name}`);
+                  }}
+                  onMouseLeave={() => {
+                    setTooltipContent("");
+                  }}
+                  onClick={() => handleMapClick(geo.id, isInDatabase)}
+                  style={{
+                    default: { fill: isInDatabase ? '#1FA2D5' : '#E5F4F7'},
+                    hover: { fill: '#095A8C' },
+                    pressed: { fill: '#095A8C' },
+                  }}
+                  stroke='#fff'
+                  strokeWidth={0.5}
+                  data-tooltip-id="world-map-tooltip"
+                  data-tooltip-content={geo.properties.name}
+                />
+              );
+            })
+          }
+        </Geographies>
+      </ComposableMap>
+    </motion.div>
   )
 }
 
